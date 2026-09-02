30e54436a61c5937d3c02b65bc88645ee63a52f0
Yoga Pratama Fajri <yogap1252002@gmail.com>
2026-09-02 15:41:03 +0700

    update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,15 @@
 export default function Hero(){
     return (
-        <></>
+        <>
+            <section id="home" className="px-5 py-16 md:py-24 lg:py-32">
+                <div>
+                    <div>
+                        <span className="text-sm font-semibold text-blue-600 bg-[#ADC6FF]">
+                            Available for work
+                        </span>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
